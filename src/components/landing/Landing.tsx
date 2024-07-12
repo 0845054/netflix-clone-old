d970787a987dedf42e6c0e2facc93bf4ce2e7249
Kayden <Kayden@test.com>
2024-07-12 10:25:26 +0800

    Finished hero1

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -9,47 +9,25 @@ const Landing = (props: Props) => {
   return (
     <div className="landing-container">
       <div className="hero-1-container">
-        <img className="hero-1-img" src={Hero} alt="missing" />
+        <img className="hero-1-img" src={Hero} alt="missing"></img>
         <div className="hero-1-info">
           <h1>Unlimited movies, TV shows, and more</h1>
-          <div className="landing-subtitle">
-            Watch anywhere. Cancel anytime.
-          </div>
-          <div className="landing-email">
+          <div className="hero-1-subtitle">Watch anywhere. Cancel anytime.</div>
+          <div className="hero-1-email-container">
             Ready to watch? Enter your email to create or restart your
             membership.
             <div className="email-input-container">
               <input
                 type="email"
                 placeholder="Enter your email"
-                className="landing-email-input"
+                className="email-input"
               />
-              <button className="landing-navBar-btn">Get Started</button>
+              <button className="hero-1-email-btn">Get Started {">"}</button>
             </div>
           </div>
         </div>
       </div>
-      <div className="hero-2-container">
-        {/* <img className="hero-1-img" src={Hero} alt="missing" /> */}
-        <div className="hero-1-info">
-          <h1>Unlimited movies, TV shows, and more</h1>
-          <div className="landing-subtitle">
-            Watch anywhere. Cancel anytime.
-          </div>
-          <div className="landing-email">
-            Ready to watch? Enter your email to create or restart your
-            membership.
-            <div className="email-input-container">
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="landing-email-input"
-              />
-              <button className="landing-navBar-btn">Get Started</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className="hero-1-container">Hero 2</div>
     </div>
   );
 };
